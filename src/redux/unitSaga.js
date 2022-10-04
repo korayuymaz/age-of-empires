73e47553d76ef8e23621029aebb5b41d4e573440
Koray Uymaz <korayuymaz@gmail.com>
2022-10-05 02:32:13 +0300

    Filters are complete

--- src/redux/unitSaga.js
+++ src/redux/unitSaga.js
@@ -29,7 +29,13 @@ function* getUnitDetails(id) {
 function* getFilteredUnits(filters) {
   let data = yield unitData.units;
   let filteredData = yield [];
-  console.log(filters.data.sliderActive);
+  let lowerCostGold = parseInt(filters.data.filters.goldCostValue[0]);
+  let upperCostGold = parseInt(filters.data.filters.goldCostValue[1]);
+  let lowerCostFood = parseInt(filters.data.filters.foodCostValue[0]);
+  let upperCostFood = parseInt(filters.data.filters.foodCostValue[1]);
+  let lowerCostWood = parseInt(filters.data.filters.woodCostValue[0]);
+  let upperCostWood = parseInt(filters.data.filters.woodCostValue[1]);
+
   // some calculations
   if (filters.data.age !== "All") {
     for (let unit in data) {
@@ -177,14 +183,18 @@ function* getFilteredUnits(filters) {
 
           if (data[unit].cost) {
             console.log("This is where its suppose to enter");
-            if (data[unit].cost.Gold && data[unit].cost.Food && data[unit].cost.Wood) {
+            if (
+              data[unit].cost.Gold &&
+              data[unit].cost.Food &&
+              data[unit].cost.Wood
+            ) {
               if (
                 lowerCostGold <= data[unit].cost.Gold &&
                 upperCostGold >= data[unit].cost.Gold &&
                 lowerCostFood <= data[unit].cost.Food &&
                 upperCostFood >= data[unit].cost.Food &&
                 lowerCostWood <= data[unit].cost.Wood &&
-                upperCostWood >= data[unit].cost.Wood 
+                upperCostWood >= data[unit].cost.Wood
               ) {
                 filteredData.push(data[unit]);
               }
@@ -196,7 +206,143 @@ function* getFilteredUnits(filters) {
       }
     }
   } else {
-    filteredData = data;
+    for (let unit in data) {
+      if (
+        filters.data.sliderActive.wood &&
+        !filters.data.sliderActive.food &&
+        !filters.data.sliderActive.gold
+      ) {
+        if (data[unit].cost) {
+          console.log("This is where its suppose to enter");
+          if (data[unit].cost.Wood) {
+            if (
+              lowerCostWood <= data[unit].cost.Wood &&
+              upperCostWood >= data[unit].cost.Wood
+            ) {
+              filteredData.push(data[unit]);
+            }
+          }
+        }
+        // If only food cost filter active
+      } else if (
+        !filters.data.sliderActive.wood &&
+        filters.data.sliderActive.food &&
+        !filters.data.sliderActive.gold
+      ) {
+        if (data[unit].cost) {
+          console.log("This is where its suppose to enter");
+          if (data[unit].cost.Food) {
+            if (
+              lowerCostFood <= data[unit].cost.Food &&
+              upperCostFood >= data[unit].cost.Food
+            ) {
+              filteredData.push(data[unit]);
+            }
+          }
+        }
+        // If only gold cost filter active
+      } else if (
+        !filters.data.sliderActive.wood &&
+        !filters.data.sliderActive.food &&
+        filters.data.sliderActive.gold
+      ) {
+        if (data[unit].cost) {
+          console.log("This is where its suppose to enter");
+          if (data[unit].cost.Gold) {
+            if (
+              lowerCostGold <= data[unit].cost.Gold &&
+              upperCostGold >= data[unit].cost.Gold
+            ) {
+              filteredData.push(data[unit]);
+            }
+          }
+        }
+        // If Wood and Food filter is active
+      } else if (
+        filters.data.sliderActive.wood &&
+        filters.data.sliderActive.food &&
+        !filters.data.sliderActive.gold
+      ) {
+        if (data[unit].cost) {
+          console.log("This is where its suppose to enter");
+          if (data[unit].cost.Food && data[unit].cost.Wood) {
+            if (
+              lowerCostFood <= data[unit].cost.Food &&
+              upperCostFood >= data[unit].cost.Food &&
+              lowerCostWood <= data[unit].cost.Wood &&
+              upperCostWood >= data[unit].cost.Wood
+            ) {
+              filteredData.push(data[unit]);
+            }
+          }
+        }
+        // If Wood and Gold filter is active
+      } else if (
+        filters.data.sliderActive.wood &&
+        !filters.data.sliderActive.food &&
+        filters.data.sliderActive.gold
+      ) {
+        if (data[unit].cost) {
+          console.log("This is where its suppose to enter");
+          if (data[unit].cost.Gold && data[unit].cost.Wood) {
+            if (
+              lowerCostGold <= data[unit].cost.Gold &&
+              upperCostGold >= data[unit].cost.Gold &&
+              lowerCostWood <= data[unit].cost.Wood &&
+              upperCostWood >= data[unit].cost.Wood
+            ) {
+              filteredData.push(data[unit]);
+            }
+          }
+        }
+        // If Food and Gold filters active
+      } else if (
+        !filters.data.sliderActive.wood &&
+        filters.data.sliderActive.food &&
+        filters.data.sliderActive.gold
+      ) {
+        if (data[unit].cost) {
+          console.log("This is where its suppose to enter");
+          if (data[unit].cost.Gold && data[unit].cost.Food) {
+            if (
+              lowerCostGold <= data[unit].cost.Gold &&
+              upperCostGold >= data[unit].cost.Gold &&
+              lowerCostFood <= data[unit].cost.Food &&
+              upperCostFood >= data[unit].cost.Food
+            ) {
+              filteredData.push(data[unit]);
+            }
+          }
+        }
+        // If all filters active
+      } else if (
+        filters.data.sliderActive.wood &&
+        filters.data.sliderActive.food &&
+        filters.data.sliderActive.gold
+      ) {
+        if (data[unit].cost) {
+          console.log("This is where its suppose to enter");
+          if (
+            data[unit].cost.Gold &&
+            data[unit].cost.Food &&
+            data[unit].cost.Wood
+          ) {
+            if (
+              lowerCostGold <= data[unit].cost.Gold &&
+              upperCostGold >= data[unit].cost.Gold &&
+              lowerCostFood <= data[unit].cost.Food &&
+              upperCostFood >= data[unit].cost.Food &&
+              lowerCostWood <= data[unit].cost.Wood &&
+              upperCostWood >= data[unit].cost.Wood
+            ) {
+              filteredData.push(data[unit]);
+            }
+          }
+        }
+      } else {
+        filteredData.push(data[unit]);
+      }
+    }
   }
   yield put({ type: SET_FILTERED_UNIT_LIST, filteredData });
 }
